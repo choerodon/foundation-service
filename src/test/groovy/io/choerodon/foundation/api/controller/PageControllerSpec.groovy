36a1138255223bb8e5a57443d10ea9ed8a92129f
陈士男 <shinan.chen@hand-china.com>
2019-04-11 17:36:07 +0800

    [ADD] page单元测试

---
+++ src/test/groovy/io/choerodon/foundation/api/controller/PageControllerSpec.groovy
@@ -0,0 +1,60 @@
+package io.choerodon.foundation.api.controller
+
+import io.choerodon.core.domain.Page
+import io.choerodon.foundation.IntegrationTestConfiguration
+import io.choerodon.foundation.api.dto.PageDTO
+import io.choerodon.foundation.api.dto.PageSearchDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Import
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author shinan.chen
+ * @since 2019/4/11
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(IntegrationTestConfiguration)
+@ActiveProfiles("test")
+class PageControllerSpec extends Specification {
+
+    @Autowired
+    TestRestTemplate restTemplate
+
+    @Shared
+    Long organizationId = 1L
+
+    def url = '/v1/organizations/{organization_id}/page'
+
+    def "pageQuery"() {
+        when: '分页查询页面列表'
+        ParameterizedTypeReference<Page<PageDTO>> typeRef = new ParameterizedTypeReference<Page<PageDTO>>() {
+        }
+        HttpEntity<PageSearchDTO> httpEntity = new HttpEntity<>(new PageSearchDTO())
+        def entity = restTemplate.exchange(url, HttpMethod.POST, httpEntity, typeRef, organizationId)
+
+        then: '状态码为200，调用成功'
+        def actRequest = false
+        def actResponseSize = 0
+        if (entity != null) {
+            if (entity.getStatusCode().is2xxSuccessful()) {
+                actRequest = true
+                if (entity.getBody() != null) {
+                    actResponseSize = entity.getBody().size()
+                }
+            }
+        }
+        actRequest == expRequest
+        actResponseSize == expResponseSize
+
+        where: '测试用例：'
+        expRequest | expResponseSize
+        true       | 2
+    }
+}
